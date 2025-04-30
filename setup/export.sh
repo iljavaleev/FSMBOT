3d3f2e2dbf0ebae5cbf897130c1a588516aa8ca5
iljavaleev <iljavaleev@gmail.com>
2025-04-30 20:34:42 +0500

    init

--- setup/export.sh
+++
@@ -1,5 +0,0 @@
-export BOT_TOKEN=7854850882:AAGILNj2vyxFp9xTT_bhf4Xp8t5EKtCfLgo
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_DB=postgres
-export POSTGRES_PORT=5432
